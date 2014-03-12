4839bb319c8ae38c965a9477e4b8a02a071bfb1b
pboechat <pboechat@gmail.com>
2014-03-11 21:29:16 -0300

    .

--- scenarios/COLLECT_STATISTICS_VS2010.bat
+++ scenarios/COLLECT_STATISTICS_VS2010.bat
@@ -33,3 +33,12 @@ Release\roadnetworkgenerator.exe 1024 768 ../../../../scenarios/rio_de_janeiro_c
 Release\roadnetworkgenerator.exe 1024 768 ../../../../scenarios/rio_de_janeiro_city_sd_4_gpu.config true true ../../../../scenarios/dump
 Release\roadnetworkgenerator.exe 1024 768 ../../../../scenarios/rio_de_janeiro_city_sd_5_gpu.config true true ../../../../scenarios/dump
 Release\roadnetworkgenerator.exe 1024 768 ../../../../scenarios/rio_de_janeiro_city_sd_6_gpu.config true true ../../../../scenarios/dump
+Release\roadnetworkgenerator.exe 1024 768 ../../../../scenarios/rio_de_janeiro_city_pr_1_gpu.config true true ../../../../scenarios/dump
+Release\roadnetworkgenerator.exe 1024 768 ../../../../scenarios/rio_de_janeiro_city_pr_2_gpu.config true true ../../../../scenarios/dump
+Release\roadnetworkgenerator.exe 1024 768 ../../../../scenarios/rio_de_janeiro_city_pr_3_gpu.config true true ../../../../scenarios/dump
+Release\roadnetworkgenerator.exe 1024 768 ../../../../scenarios/rio_de_janeiro_city_pr_4_gpu.config true true ../../../../scenarios/dump
+Release\roadnetworkgenerator.exe 1024 768 ../../../../scenarios/rio_de_janeiro_city_pr_5_gpu.config true true ../../../../scenarios/dump
+Release\roadnetworkgenerator.exe 1024 768 ../../../../scenarios/rio_de_janeiro_city_pr_6_gpu.config true true ../../../../scenarios/dump
+Release\roadnetworkgenerator.exe 1024 768 ../../../../scenarios/rio_de_janeiro_city_pr_7_gpu.config true true ../../../../scenarios/dump
+Release\roadnetworkgenerator.exe 1024 768 ../../../../scenarios/rio_de_janeiro_city_pr_8_gpu.config true true ../../../../scenarios/dump
+Release\roadnetworkgenerator.exe 1024 768 ../../../../scenarios/rio_de_janeiro_city_pr_9_gpu.config true true ../../../../scenarios/dump
